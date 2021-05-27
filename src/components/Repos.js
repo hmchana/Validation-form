82519f1b0e9fc2841b10baa21950bd9c1531517e
Hamza Chana <hmchana96@gmail.com>
2021-05-27 02:04:46 +0100

    implement pagination

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,4 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+  forwardRef
+} from 'react';
 import Loader from 'react-loader-spinner';
 import axios from 'axios';
 import Repo from './Repo';
@@ -6,9 +12,26 @@ import { Card, Alert } from 'antd';
 
 const Repos = () => {
   const [repos, setRepos] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState({ type: '', message: '' });
   const [showAlert, setShowAlert] = useState(false);
+
+  const observer = useRef();
+  const lastRepoRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPageNumber((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading]
+  );
+
   useEffect(() => {
     const fetchRepos = async () => {
       try {
@@ -16,9 +39,11 @@ const Repos = () => {
         const {
           data: { items }
         } = await axios.get(
-          `https://api.github.com/search/repositories?q=created:>2017-11-22&sort=stars&order=desc`
+          `https://api.github.com/search/repositories?q=created:>2017-11-22&sort=stars&order=desc&page=${pageNumber}`
         );
-        setRepos(items);
+        setRepos((prevRepos) => {
+          return [...prevRepos, ...items];
+        });
         setLoading(false);
       } catch (error) {
         setLoading(false);
@@ -31,16 +56,28 @@ const Repos = () => {
     };
     fetchRepos();
     // eslint-disable-next-line
-  }, []);
+  }, [pageNumber]);
 
   console.log(repos);
 
-  const Repos = repos.map((repo) => {
-    return (
-      <Card key={repo.id} style={{ width: 600, height: 300 }}>
-        <Repo repo={repo} />
-      </Card>
-    );
+  const Repos = repos.map((repo, index) => {
+    if (repos.length === index + 1) {
+      return (
+        <div key={repo.id} ref={lastRepoRef}>
+          <Card style={{ width: 600, height: 300 }}>
+            <Repo repo={repo} />
+          </Card>
+        </div>
+      );
+    } else {
+      return (
+        <div key={repo.id}>
+          <Card style={{ width: 600, height: 300 }}>
+            <Repo repo={repo} />
+          </Card>
+        </div>
+      );
+    }
   });
 
   return (
@@ -55,7 +92,7 @@ const Repos = () => {
         />
       )}
       <div>
-        {loading ? (
+        {/* {loading ? (
           <Loader
             type='Oval'
             color='#606f7d'
@@ -71,6 +108,24 @@ const Repos = () => {
           />
         ) : (
           Repos
+        )} */}
+        {Repos}
+      </div>
+      <div>
+        {loading && (
+          <Loader
+            type='Oval'
+            color='#606f7d'
+            height={36}
+            width={36}
+            style={{
+              width: '20px',
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              marginTop: '20px'
+            }}
+          />
         )}
       </div>
     </React.Fragment>
